59674f67be0019f65c4408eb792ef3653159c317
Tomasz Struczyński <tomasz.struczynski@socializer.pl>
2016-10-18 16:34:41 +0200

    Added list workers command.

---
+++ src/Gendoria/CommandQueueBundle/Tests/Command/ListWorkersCommandTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Gendoria\CommandQueueBundle\Tests\Command;
+
+use Gendoria\CommandQueueBundle\Command\ListWorkersCommand;
+use Gendoria\CommandQueueBundle\Worker\WorkerRunnerInterface;
+use Gendoria\CommandQueueBundle\Worker\WorkerRunnerManager;
+use PHPUnit_Framework_TestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Test run worker command.
+ *
+ * @author Tomasz Struczyński <t.struczynski@gmail.com>
+ */
+class ListWorkersCommandTest extends PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        $container = new ContainerBuilder();
+        $kernel = $this->getMockBuilder(KernelInterface::class)->getMock();
+        $kernel->expects($this->any())->method('getBundles')->will($this->returnValue(array()));
+        $kernel->expects($this->any())->method('getContainer')->will($this->returnValue($container));
+        
+        $manager = new WorkerRunnerManager($container);
+        $container->set('gendoria_command_queue.runner_manager', $manager);
+        $runner = $this->getMockBuilder(WorkerRunnerInterface::class)->getMock();
+        $container->set('test_svc', $runner);
+        $manager->addRunner('different', 'test_svc');
+        
+        $application = new Application($kernel);
+        $application->add(new ListWorkersCommand());
+
+        $command = $application->find('command-queue:list-workers');
+        $commandTester = new CommandTester($command);
+        $exitCode = $commandTester->execute(array());
+        $this->assertEquals(0, $exitCode);
+        $this->assertContains('different', $commandTester->getDisplay());
+    }    
+}
